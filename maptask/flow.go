6f4aa527152a57c49652005dfe0699a97a84ff13
Sergey <xxsmotur@gmail.com>
2019-02-07 16:53:36 +0500

    init for the flow

---
+++ maptask/flow.go
@@ -0,0 +1,11 @@
+package maptask
+
+// Flow defines the main flow for the app
+type Flow struct {
+}
+
+// NewText provides initialization of the flow
+// with a text
+func NewText(text string) *Flow {
+	return &Flow{}
+}
