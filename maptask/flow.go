5633908248f28536765a9cf14c641c5294ec10bd
Sergey <xxsmotur@gmail.com>
2019-02-07 17:13:04 +0500

    init of steps

--- maptask/flow.go
+++ maptask/flow.go
@@ -2,21 +2,23 @@ package maptask
 
 // Flow defines the main flow for the app
 type Flow struct {
+	steps []*Step
 }
 
 // NewText provides initialization of the flow
 // with a text
 func NewText(text string) *Flow {
-	return &Flow{}
+	return &Flow{
+		steps: []*Step{},
+	}
 }
 
 // Map provides mapping of the function
-func (f* Flow) Map(name string, fn func(interface{}) error) *Flow{
+func (f *Flow) Map(name string, fn func(interface{}) error) *Flow {
 	return f
 }
 
 // Reduce
-func (f* Flow) Reduce(name string, fn func(interface{}) error) *Flow{
+func (f *Flow) Reduce(name string, fn func(interface{}) error) *Flow {
 	return f
 }
-
