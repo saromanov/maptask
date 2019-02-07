df3577f5b05225b3525d81cab7bdf39bd8e8ecb3
Sergey <xxsmotur@gmail.com>
2019-02-07 17:15:55 +0500

    fixing of arguments

--- maptask/step.go
+++ maptask/step.go
@@ -2,6 +2,8 @@ package maptask
 
 // Step represents flow step
 type Step struct {
-	Name string
-	Fn   func([]interface{}) error
+	ID     string
+	Name   string
+	Fn     func([]interface{}) error
+	Params map[string]interface{}
 }
