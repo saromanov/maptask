425eac8e586c81002144191e7196d7a79cef0bbb
Sergey <xxsmotur@gmail.com>
2019-02-07 16:19:41 +0500

    Add comments

--- agent/topology.go
+++ agent/topology.go
@@ -6,18 +6,21 @@ import "sync"
 type AgentInfo struct {
 }
 
+// Stand contains group of Agents
 type Stand struct {
 	sync.RWMutex
 	Name   string
 	Agents map[string]*AgentInfo
 }
 
+// DataCenter contains group of Stands
 type DataCenter struct {
 	sync.RWMutex
 	Name   string
 	Stands map[string]*Stand
 }
 
+// Topology defines representation of datacenters
 type Topology struct {
 	sync.RWMutex
 	DataCenters map[string]*DataCenter
