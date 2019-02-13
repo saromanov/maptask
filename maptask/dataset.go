ab25e7a116d458cc1db55bb7700e9fc15e788bd7
Sergey <xxsmotur@gmail.com>
2019-02-13 17:05:05 +0500

    initialization for datasets

---
+++ maptask/dataset.go
@@ -0,0 +1,14 @@
+package maptask
+
+// Dataset implements representation of the tasks
+type Dataset struct {
+	Flow *Flow
+}
+
+// newDataset provides initialization of the dataset
+func newDataset(f *Flow) *Dataset {
+	d := &Dataset{
+		Flow: context,
+	}
+	return d
+}
