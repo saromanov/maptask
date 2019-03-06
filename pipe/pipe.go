6503ed7bb3988364ffa6fde6a93400f4f707ff90
Sergey <xxsmotur@gmail.com>
2019-03-06 18:30:11 +0500

    register of the floe step

--- pipe/pipe.go
+++ pipe/pipe.go
@@ -2,16 +2,21 @@
 // into distributed execution. It
 package pipe
 
-import "sync"
+import (
+	"sync"
+	"github.com/saromanov/maptask/proto"
+)
 
 // Pipe defines struct for mapping maptask iinto distributed
 type Pipe struct {
 	mu *sync.Mutex
+	steps []*proto.FlowStep
 }
 
 // New provides initialization of the Pipe
-func New() *Pipe {
+func New(steps []*proto.FlowStep) *Pipe {
 	return &Pipe{
 		mu: &sync.Mutex{},
+		steps: steps,
 	}
 }
