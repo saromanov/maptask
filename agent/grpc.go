15a2e039dd77d874e418c2039d6c632749e59e70
Sergey <xxsmotur@gmail.com>
2019-03-13 18:03:32 +0500

    fixing of import

--- agent/grpc.go
+++ agent/grpc.go
@@ -3,11 +3,12 @@ package agent
 import (
 	"net"
 	"google.golang.org/grpc"
+	"github.com/saromanov/maptask/proto"
 )
 
 // serverGRPC provides serving of the grpc server
 func (aa *Agent) serveGRPC(listener net.Listener) {
 	grpcServer := grpc.NewServer()
-	pb.RegisterMapTaskServer(grpcServer, as)
+	proto.RegisterMapTaskServer(grpcServer, as)
 	grpcServer.Serve(listener)
 }
