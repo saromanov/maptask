6d4b9ae51cfa991b4679f280bcc55f7b92a31907
Sergey <xxsmotur@gmail.com>
2019-03-14 16:55:15 +0500

    Checking of the length of flow tasks before init of the pipe

--- pipe/pipe.go
+++ pipe/pipe.go
@@ -3,20 +3,26 @@
 package pipe
 
 import (
+	"errors"
 	"sync"
-	"github.com/saromanov/maptask/proto"
+	pb "github.com/saromanov/maptask/proto"
 )
+
+var errNoSteps = errors.New("steps is not defined")
 
 // Pipe defines struct for mapping maptask iinto distributed
 type Pipe struct {
 	mu *sync.Mutex
-	steps []*proto.FlowStep
+	steps []*pb.FlowStep
 }
 
 // New provides initialization of the Pipe
-func New(steps []*proto.FlowStep) *Pipe {
+func New(steps []*pb.FlowStep) (*Pipe, error) {
+	if len(steps) == 0 {
+		return nil, errNoSteps
+	}
 	return &Pipe{
 		mu: &sync.Mutex{},
 		steps: steps,
-	}
+	}, nil
 }
